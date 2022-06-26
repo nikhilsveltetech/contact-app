26872456f4b8725ecccdcbc5a5d2e135b47bf1fb
nikhil0162 <nikhil0162@gmail.com>
2022-06-26 22:09:31 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header.js'
+import './ContactList.js'
+import './AddContact.js'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+
+}
+
+export default App;
